ee65d7afb941fffc77a97ed047fdc85638b83a75
Lemayan Leleina <lemayanleleina@gmail.com>
2026-06-04 15:10:47 +0300

    chore: fix .gitignore, remove .next build artifacts from tracking

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/Overlay\":{\"id\":\"app\\\\page.tsx -> @/components/Overlay\",\"files\":[\"static/chunks/_app-pages-browser_src_components_Overlay_tsx.js\"]}}"
